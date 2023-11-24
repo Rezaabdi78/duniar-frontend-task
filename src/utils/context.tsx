ca6f6743c05900ef4c64a5f08a4706f4e071ea46
rezaabdi78999 <reza78999@gmail.com>
2023-11-24 14:01:06 +0330

    updated contexty initialization to be an empty aray

--- src/utils/context.tsx
+++ src/utils/context.tsx
@@ -3,15 +3,7 @@ import { ITask, ITaskAction, ITaskListContextProps } from "./interfaces";
 
 const TaskListContext = createContext<ITaskListContextProps>({
 	setState: () => {},
-	state: [
-		{
-			id: "1",
-			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio morbi quis commodo odio aenean. Vehicula ipsum a arcu cursus vitae congue. Nec ullamcorper sit amet risus nullam eget felis. Ac auctor augue mauris augue. Dui nunc mattis enim ut tellus elementum sagittis vitae et. Malesuada nunc vel risus commodo viverra maecenas. Ac turpis egestas maecenas pharetra. Varius morbi enim nunc faucibus a. Metus dictum at tempor commodo ullamcorper a lacus. Cursus eget nunc scelerisque viverra mauris. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Ridiculus mus mauris vitae ultricies.",
-			status: "todo",
-			title: "Task number one",
-			history: [],
-		},
-	],
+	state: [],
 });
 
 export const TaskContextProvider = ({
@@ -19,36 +11,7 @@ export const TaskContextProvider = ({
 }: {
 	children: JSX.Element[] | JSX.Element;
 }) => {
-	const [state, setState] = useState<ITask[]>([
-		{
-			id: "1",
-			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio morbi quis commodo odio aenean. Vehicula ipsum a arcu cursus vitae congue. Nec ullamcorper sit amet risus nullam eget felis. Ac auctor augue mauris augue. Dui nunc mattis enim ut tellus elementum sagittis vitae et. Malesuada nunc vel risus commodo viverra maecenas. Ac turpis egestas maecenas pharetra. Varius morbi enim nunc faucibus a. Metus dictum at tempor commodo ullamcorper a lacus. Cursus eget nunc scelerisque viverra mauris. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Ridiculus mus mauris vitae ultricies.",
-			status: "todo",
-			title: "Task number one",
-			history: [],
-		},
-		{
-			id: "2",
-			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio morbi quis commodo odio aenean. Vehicula ipsum a arcu cursus vitae congue. Nec ullamcorper sit amet risus nullam eget felis. Ac auctor augue mauris augue. Dui nunc mattis enim ut tellus elementum sagittis vitae et. Malesuada nunc vel risus commodo viverra maecenas. Ac turpis egestas maecenas pharetra. Varius morbi enim nunc faucibus a. Metus dictum at tempor commodo ullamcorper a lacus. Cursus eget nunc scelerisque viverra mauris. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Ridiculus mus mauris vitae ultricies.",
-			status: "todo",
-			title: "Task number one",
-			history: [],
-		},
-		{
-			id: "3",
-			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio morbi quis commodo odio aenean. Vehicula ipsum a arcu cursus vitae congue. Nec ullamcorper sit amet risus nullam eget felis. Ac auctor augue mauris augue. Dui nunc mattis enim ut tellus elementum sagittis vitae et. Malesuada nunc vel risus commodo viverra maecenas. Ac turpis egestas maecenas pharetra. Varius morbi enim nunc faucibus a. Metus dictum at tempor commodo ullamcorper a lacus. Cursus eget nunc scelerisque viverra mauris. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Ridiculus mus mauris vitae ultricies.",
-			status: "todo",
-			title: "Task number one",
-			history: [],
-		},
-		{
-			id: "4",
-			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio morbi quis commodo odio aenean. Vehicula ipsum a arcu cursus vitae congue. Nec ullamcorper sit amet risus nullam eget felis. Ac auctor augue mauris augue. Dui nunc mattis enim ut tellus elementum sagittis vitae et. Malesuada nunc vel risus commodo viverra maecenas. Ac turpis egestas maecenas pharetra. Varius morbi enim nunc faucibus a. Metus dictum at tempor commodo ullamcorper a lacus. Cursus eget nunc scelerisque viverra mauris. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Ridiculus mus mauris vitae ultricies.",
-			status: "todo",
-			title: "Task number one",
-			history: [],
-		},
-	]);
+	const [state, setState] = useState<ITask[]>([]);
 
 	return (
 		<TaskListContext.Provider value={{ state, setState }}>
