e973e762c116742472891d3e0bbb1ced02b82b06
rezaabdi78999 <reza78999@gmail.com>
2023-11-24 09:48:39 +0330

    minor updates in pages and context

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,7 +1,13 @@
 export interface ITask {
+	id: string;
 	title: string;
-	status: "todo" | "blocked" | "inProgress" | "inQA";
+	status: "todo" | "blocked" | "inProgress" | "inQA" | "done" | "deployed";
 	desc: string;
+	history: Array<{
+		prev: Partial<Omit<ITask, "id" | "history">>;
+		new: Partial<Omit<ITask, "id" | "history">>;
+		time: string;
+	}>;
 }
 
 export interface ITaskAction extends Partial<ITask> {}
