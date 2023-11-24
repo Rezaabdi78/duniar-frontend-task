f1a41bb3175d55bb0ce13106f4d8812337aecf96
rezaabdi78999 <reza78999@gmail.com>
2023-11-24 13:59:36 +0330

    include add task modal in  all tasks view

--- src/components/Sections/TodoList/index.tsx
+++ src/components/Sections/TodoList/index.tsx
@@ -1,22 +1,31 @@
 import { useTaskContext } from "@/utils/context";
 import { TodoItem } from "../todoItem/todoItem";
-import { SimpleGrid, VStack } from "@chakra-ui/react";
+import { SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import { AddTaskModal } from "./addTaskModal";
+import { ReactIcon } from "@chakra-ui/icons";
 
 export const TodoList = () => {
 	const { state, setState } = useTaskContext();
 
-	return (
+	return state.length ? (
 		<SimpleGrid
 			p={4}
 			w="full"
 			spacing={4}
 			templateColumns="repeat(auto-fill, minmax(260px, 1fr))"
+			templateRows="repeat(auto-fill, minmax(260px, 1fr))"
 		>
 			<AddTaskModal />
 			{state.map((data) => (
 				<TodoItem data={data} key={data.id} />
 			))}
 		</SimpleGrid>
+	) : (
+		<VStack spacing={12} mt="20%">
+			<Text fontSize="3xl" fontWeight={700}>
+				You don&apos;t have any tasks yet. Take a nap <ReactIcon />
+			</Text>
+			<AddTaskModal />
+		</VStack>
 	);
 };
